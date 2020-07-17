f9b6e327882d222b5fbc2add853656bc284d9047
Sean Fu <sean14xx@gmail.com>
2020-07-17 13:20:39 +0200

    refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import Gif from './Gif';
+import GifList from './GifList';
 
 class App extends Component {
   render () {
+    const gifs = [
+      { id: "v6aOjy0Qo1fIA" },
+      { id: "vFKqnCdLPNOKc" }
+    ];
     return (
       <div>
         <div className="left-scene">
@@ -12,7 +17,9 @@ class App extends Component {
             <Gif id="v6aOjy0Qo1fIA" />
           </div>
         </div>
-        <div className="right-scene" />
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import Gif from './Gif';
 
 class GifList extends Component {
+  renderList = () => {
+    return this.props.gifs.map((gif) => {
+      return <Gif id={gif.id} key={gif.id} />;
+    });
+  }
+
   render () {
     return (
-
-    )
+      <div className="gif-list">
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
