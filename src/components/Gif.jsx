e80e638c9cce876f1b928808f22def2f8d70d6d7
Sean Fu <sean14xx@gmail.com>
2020-07-25 20:24:05 +0200

    pass gif id to selectGif function when user click specific gif

--- src/components/Gif.jsx
+++ src/components/Gif.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handleClick = () => {
+    this.props.selectGif(this.props.id);
+  };
+
   render () {
     const src = `https://media3.giphy.com/media/${this.props.id}/200.gif`;
+    console.log(this.props.id);
     return (
-      <img src={src} alt="" className="gif" />
+      <img
+        src={src}
+        alt=""
+        className="gif"
+        onClick={this.handleClick}
+      />
     );
   }
 }
