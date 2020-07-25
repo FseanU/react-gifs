da0a574c574e75f5a1f1070b5eb88d4c219ec5aa
Sean Fu <sean14xx@gmail.com>
2020-07-25 20:18:37 +0200

    send request to giphy api when user type by adding onChange event

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (e) => {
+    this.props.searchFunction(e.target.value);
+  }
+
   render () {
     return (
-      <input type="text" className="form-control form-search" />
+      <input
+        type="text"
+        className="form-control form-search"
+        onChange={this.handleUpdate}
+      />
     );
   }
 }
