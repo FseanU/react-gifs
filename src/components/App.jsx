5762e4a278237cfb3c58015b127d73617c076136
Sean Fu <sean14xx@gmail.com>
2020-07-25 20:21:33 +0200

    add slelectGif property for setting the state of selectedGifId

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 import SearchBar from './SearchBar';
 import Gif from './Gif';
 import GifList from './GifList';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [ ],
+      selectedGifId: "v6aOjy0Qo1fIA"
+    };
+  }
+
+  search = (query) => {
+    giphy('3oRZIiI2kfRptrUyBHSi0cKwZ95rcneB').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render () {
-    const gifs = [
-      { id: "v6aOjy0Qo1fIA" },
-      { id: "vFKqnCdLPNOKc" }
-    ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="v6aOjy0Qo1fIA" />
+            <Gif id={this.state.selectedGifId} selectGif={this.selectGif} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
